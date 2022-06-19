f3e141477f573f2951d4c8d1fad6b83d9e891787
guberger@PC-1r0-013 <guillaume.berger@uclouvain.be>
2022-06-19 09:46:43 -0600

    Verifier Pos

--- src/clf.py
+++ src/clf.py
@@ -1,6 +1,11 @@
 import numpy as np
 from gurobipy import gurobipy, GRB, abs_
-from src.symbolics import evalf_expr
+import z3
+from src.symbolics import evalf_expr, diff_expr
+from src.z3utils import \
+    create_z3syms_from_spsyms, \
+    convert_spexpr_to_z3expr, \
+    get_vars_from_z3model
 
 class ExprTerm:
     def __init__(self, expr_V, expr_DVF, expr_DVGs) -> None:
@@ -82,3 +87,66 @@ class Generator:
         coeffs_opt = np.array([coeff.X for coeff in coeffs])
         r_opt = model.getObjective().getValue()
         return coeffs_opt, r_opt
+
+## Verifier
+
+class VerifierError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+class Verifier:
+    def __init__(
+            self, syms, lbs, ubs, exprterms, ninp, tol_pos, tol_lie
+        ) -> None:
+        assert len(syms) == len(lbs) == len(ubs)
+        self.spsyms = syms
+        self.lbs = lbs
+        self.ubs = ubs
+        self.exprterms = exprterms
+        self.ninp = ninp
+        self.tol_pos = tol_pos
+        self.tol_lie = tol_lie
+
+    def _check_pos_single(self, expr_V, ifix, lufix):
+        ctx = z3.Context()
+        solver = z3.Solver(ctx=ctx)
+        z3syms, syms_map = \
+            create_z3syms_from_spsyms(ctx, self.spsyms)
+        # z3r = [z3.Real(f'r{i}', ctx=ctx) for i in range(self.ninp)]
+
+        for i in range(len(z3syms)):
+            if i != ifix:
+                solver.add(z3syms[i] >= self.lbs[i])
+                solver.add(z3syms[i] <= self.ubs[i])
+            elif i == ifix:
+                if lufix == -1:
+                    solver.add(z3syms[i] == self.lbs[i])
+                elif lufix == 1:
+                    solver.add(z3syms[i] == self.ubs[i])
+                else:
+                    raise VerifierError(
+                        'Unknow lufix = %s.' % str(lufix)
+                    )
+
+        z3expr = convert_spexpr_to_z3expr(syms_map, expr_V)
+        solver.add(z3expr <= self.tol_pos)
+
+        res = solver.check()
+
+        if res == z3.sat:
+            model = solver.model()
+            vars_ = get_vars_from_z3model(syms_map, model)
+            vars = np.array([vars_[sym.name] for sym in self.spsyms])
+            return False, vars
+        else:
+            return True, np.zeros(len(self.spsyms))
+
+    def check_pos(self, coeffs):
+        exprs_term = np.array([exprterm.V for exprterm in self.exprterms])
+        expr_V = np.dot(coeffs, exprs_term)
+        for ifix in range(len(self.spsyms)):
+            for lufix in (-1, 1):
+                res, vars = self._check_pos_single(expr_V, ifix, lufix)
+                if not res:
+                    return False, vars
+        return True, np.zeros(len(self.spsyms))
--- test/test_clf.py
+++ test/test_clf.py
@@ -4,7 +4,7 @@ import numpy as np
 import sympy as sp
 import gurobipy
 from src.symbolics import evalf_expr
-from src.clf import ExprTerm, Generator
+from src.clf import ExprTerm, Generator, Verifier
 
 class TestGenerator(unittest.TestCase):
     def __init__(self, methodName: str = ...) -> None:
@@ -31,3 +31,31 @@ class TestGenerator(unittest.TestCase):
         self.assertEqual(round(coeffs[0], 3), 0.4)
         self.assertAlmostEqual(coeffs[1], 1)
         self.assertEqual(round(r, 3), 0.2)
+
+class TestVerifier(unittest.TestCase):
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName)
+        x, y = syms = np.array(sp.symbols('x y'))
+        lbs = np.array([-1, -1])
+        ubs = np.array([1, 1])
+        exprterms = [
+            ExprTerm(x**2, 2*x*(x/2 + y), [2*x*x, 0*y]),
+            ExprTerm(y**2, 2*y*y/2, [0*x, 2*y*y])
+        ]
+        ninp = 2
+        tol_pos = 0.1
+        tol_lie = 0.1
+
+        self.verif = Verifier(syms, lbs, ubs, exprterms, ninp, tol_pos, tol_lie)
+
+    def test_check(self):
+        coeffs = np.array([1, 0.10000000001])
+        res, vars = self.verif.check_pos(coeffs)
+        print(res)
+        print(vars)
+        # self.assertFalse(self.res1)
+        # val_expr1 = evalf_expr(self.expr1, self.syms, self.vars1)
+        # self.assertTrue(self.p.contains(self.vars2))
+        # self.assertLessEqual(round(val_expr1, 1), 0)
+        # self.assertTrue(self.res2)
+        pass
